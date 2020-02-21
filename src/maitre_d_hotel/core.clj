79bf795e5a9e5c6a1baaf21f5085539eeb339413
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-02-21 10:18:19 +0100

    r Supprime la duplication du livre vide

--- src/maitre_d_hotel/core.clj
+++ src/maitre_d_hotel/core.clj
@@ -1,12 +1,13 @@
 (ns maitre-d-hotel.core)
 
 (def nombre-de-places 12)
-(def livre (atom {:places-restantes nombre-de-places}))
+(def livre-vide {:places-restantes nombre-de-places})
+(def livre (atom livre-vide))
 
 (defn efface-livre []
-  (swap! livre assoc :places-restantes nombre-de-places))
+  (reset! livre livre-vide))
 
-(defn traite-demande [livre places]
+(defn ^:private traite-demande [livre places]
   (let [différence (- (:places-restantes livre) places)]
     (if (>= différence 0)
       (assoc livre :places-restantes différence)
@@ -15,4 +16,4 @@
 (defn réserve-table
   ([places date]
    (let [[ancien nouveau] (swap-vals! livre traite-demande places)]
-     {:accepté (not (= ancien nouveau))})))
+     {:accepté (not= ancien nouveau)})))
