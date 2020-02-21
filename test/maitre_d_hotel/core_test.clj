8e503d53b31ba044c5e6d25b6007e01d4f80b1ed
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-02-21 13:27:27 +0100

    r Extrait la variable de test `aujourd'hui`

--- test/maitre_d_hotel/core_test.clj
+++ test/maitre_d_hotel/core_test.clj
@@ -11,6 +11,8 @@
 ;; ok: Une table pour une personne aujourd'hui (table de un déjà réservée à une autre date)
 ;; ok: Une table pour une personne aujourd'hui (table de douze déjà réservée à une autre date)
 
+(def aujourd'hui "2020-02-20")
+
 (defn setup [f]
   (efface-livre)
   (f))
@@ -18,22 +20,22 @@
 (use-fixtures :each setup)
 
 (deftest une-table-pour-1-aujourd'hui
-    (is (true? (:accepté (réserve-table 1 "2020-02-20")))))
+    (is (true? (:accepté (réserve-table 1 aujourd'hui)))))
 
 (deftest Une-table-pour-12-aujourd'hui
-    (is (true? (:accepté (réserve-table 12 "2020-02-20")))))
-
-(deftest une-table-pour-2-puis-2-aujourd'hui
-  (réserve-table 2 "2020-02-20")
-  (is (true? (:accepté (réserve-table 2 "2020-02-20")))))
-
-(deftest une-table-pour-3-puis-3-aujourd'hui
-  (réserve-table 3 "2020-02-20")
-  (is (true? (:accepté (réserve-table 3 "2020-02-20")))))
+    (is (true? (:accepté (réserve-table 12 aujourd'hui)))))
 
 (deftest une-table-pour-13-aujourd'hui
-  (is (false? (:accepté (réserve-table (inc nombre-de-places) "2020-02-20")))))
+  (is (false? (:accepté (réserve-table (inc nombre-de-places) aujourd'hui)))))
+
+(deftest une-table-pour-2-puis-2-aujourd'hui
+  (réserve-table 2 aujourd'hui)
+  (is (true? (:accepté (réserve-table 2 aujourd'hui)))))
+
+(deftest une-table-pour-3-puis-3-aujourd'hui
+  (réserve-table 3 aujourd'hui)
+  (is (true? (:accepté (réserve-table 3 aujourd'hui)))))
 
 (deftest une-table-pour-12-puis-une-pour-1-aujourd'hui
-  (réserve-table nombre-de-places "2020-02-20")
-  (is (false? (:accepté (réserve-table 1 "2020-02-20")))))
+  (réserve-table nombre-de-places aujourd'hui)
+  (is (false? (:accepté (réserve-table 1 aujourd'hui)))))
